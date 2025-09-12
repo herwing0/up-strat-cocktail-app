6a6c6db7a9643f771177fa0452ddcf67845d56ab
herwing0 <penarandajn@hotmail.com>
2025-09-12 00:35:44 -0300

    (fix) maximum budget increment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { routes } from './app/app.routes';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule) // requerido por PrimeNG
+  ]
+}).catch(err => console.error(err));
